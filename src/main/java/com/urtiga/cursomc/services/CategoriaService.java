6ad0b1cfc0f8b887bd77e45aeff5b943dbfa6a67
Severino Urtiga <urtigah@gmail.com>
2018-08-11 23:37:18 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/urtiga/cursomc/services/CategoriaService.java
+++ src/main/java/com/urtiga/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.urtiga.cursomc.domain.Categoria;
 import com.urtiga.cursomc.repositories.CategoriaRepository;
+import com.urtiga.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + 
+												Categoria.class.getName());
+		}
 		return obj;
 	}
 
